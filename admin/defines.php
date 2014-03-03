545ba6d3d003ab53df7f65611e9ad69e75f5f55d
Oleg Seyfetdinov <olegshs@gmail.com>
2014-03-03 17:24:48 +0400

    Version 0.9.8

--- admin/defines.php
+++ admin/defines.php
@@ -9,7 +9,7 @@ defined('_JEXEC') or die('Restricted access');	// No direct access
 if (!defined('DS'))
 	define('DS', DIRECTORY_SEPARATOR);
 
-define( 'globalflash_version',		'0.9.7' );
+define( 'globalflash_version',		'0.9.8' );
 define( 'globalflash_debug',		false );
 
 $jversion = new JVersion();
